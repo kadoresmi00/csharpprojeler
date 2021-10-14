ca2a0c77031d31533c7c98f743357f58c13b53b2
Kado <kadoresmi00@gmail.com>
2021-10-14 14:43:12 +0300

    Add files via upload

---
+++ form app - for sayılar arası toplama/for hesaplama/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for_hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int toplam = 0;
+            int start = Convert.ToInt32(textBox1.Text);
+            int stop = Convert.ToInt32(textBox2.Text);
+            for (int i = start; i < stop; i++)
+            {
+                toplam = toplam + i;
+            }
+            MessageBox.Show("Sonuç: "+toplam.ToString(),"For Toplam Sayı Hesaplama");
+        }
+    }
+}
