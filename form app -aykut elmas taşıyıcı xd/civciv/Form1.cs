44b0f38692421e7184eb0b5b211ce5e4e054de93
Kado <kadoresmi00@gmail.com>
2021-10-14 11:56:40 +0300

    Add files via upload

---
+++ form app -aykut elmas taşıyıcı xd/civciv/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace civciv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = pictureBox1.Left;
+            i += 10;
+            pictureBox1.Left = i;
+            if (i >= 570)
+            {
+                pictureBox1.Left = 12;                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i = pictureBox1.Left;
+            i += 25;
+            pictureBox1.Left = i;
+            if (i >= 570)
+            {
+                pictureBox1.Left = 12;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i = pictureBox2.Left;
+            i += 10;
+            pictureBox2.Left = i;
+            if (i >= 570)
+            {
+                pictureBox2.Left = 12;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int i = pictureBox2.Left;
+            i += 25;
+            pictureBox2.Left = i;
+            if (i >= 570)
+            {
+                pictureBox2.Left = 12;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Left = 12;
+            pictureBox2.Left = 12;
+        }
+    }
+}
