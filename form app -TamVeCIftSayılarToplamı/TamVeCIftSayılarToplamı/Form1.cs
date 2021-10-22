4cff93a55632a2cf62a50e686eb5a0ae76f27ead
Kado <kadoresmi00@gmail.com>
2021-10-22 10:21:25 +0300

    Add files via upload

---
+++ form app -TamVeCIftSayılarToplamı/TamVeCIftSayılarToplamı/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TamVeCIftSayılarToplamı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int tamSayi = 1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tamSayi = 1;
+            for (int i = 1; i < 100; i++)
+            {
+                tamSayi = tamSayi + i++;
+            }
+            textBox1.Text = tamSayi.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tamSayi = 1;
+            for (int i = 1; i < 100; i+=2)
+            {
+                tamSayi = tamSayi + i++;
+            }
+            textBox1.Text = tamSayi.ToString();
+        }
+    }
+}
